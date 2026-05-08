642de9abbc0b65a4c8fe09159973aea66bcf4212
exyone <exyone@88.com>
2026-05-08 20:25:50 +0800

    add google webmaster_verifications

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1778218558',resources: [
+    cacheName: 'chirpy-1778243090',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
